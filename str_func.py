29aab0f6f79cf874cad5a2765637adfd101d25b3
Dmitriy Nadelyaev <kopstant99@yandex.ru>
2024-02-18 11:39:21 +0900

    add func upper_first_letter and docstring for all

--- str_func.py
+++ str_func.py
@@ -1,6 +1,16 @@
 def upper_text():
+    """Делает заглавными буквами весь введеный текст"""
     string = input()
     str_upper = string.upper()
     print(str_upper)
 
 upper_text()
+
+
+def upper_first_letter():
+    """Делает заглавной первую букву каждого введеного слова"""
+    string_letter = input()
+    str_upper_letter = string_letter.title()
+    print(str_upper_letter)
+
+upper_first_letter()
